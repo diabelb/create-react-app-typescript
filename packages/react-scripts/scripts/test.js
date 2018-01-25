55d5f12d09ee2719f2777a312145e7e458fdf945
dawidboruta <diabelb@gmail.com>
2018-01-25 22:51:26 +0100

    removed node 6 from travis (not supported Object.values)

--- packages/react-scripts/scripts/test.js
+++ packages/react-scripts/scripts/test.js
@@ -8,12 +8,6 @@
 // @remove-on-eject-end
 'use strict';
 
-let values = require('object.values');
-
-if (!Object.values) {
-  values.shim();
-}
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
